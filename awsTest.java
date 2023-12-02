887ad349359b92cce80085321ea0fdb1c750ecf7
jonghyeon <ditlswlwhs3@gmail.com>
2023-12-02 20:01:41 +0900

    기본 기능 정검 및 condor_status, 추가기능(key, 보안그룹등) 추가

--- awsTest.java
+++ awsTest.java
@@ -1,5 +1,3 @@
-package aws;
-
 /*
 * Cloud Computing
 * 
@@ -7,25 +5,44 @@ package aws;
 * using AWS Java SDK Library
 * 
 */
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Iterator;
 import java.util.Scanner;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.dynamodbv2.datamodeling.KeyPair;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
 import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.DescribeKeyPairsRequest;
+import com.amazonaws.services.ec2.model.DescribeKeyPairsResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsRequest;
+import com.amazonaws.services.ec2.model.DescribeSecurityGroupsResult;
+import com.amazonaws.services.ec2.model.SecurityGroup;
 import com.amazonaws.services.ec2.model.Region;
+import com.amazonaws.services.ec2.model.AmazonEC2Exception;
 import com.amazonaws.services.ec2.model.AvailabilityZone;
 import com.amazonaws.services.ec2.model.DryRunSupportedRequest;
 import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.TerminateInstancesResult;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 import com.amazonaws.services.ec2.model.StartInstancesRequest;
 import com.amazonaws.services.ec2.model.InstanceType;
+import com.amazonaws.services.ec2.model.KeyPairInfo;
+import com.amazonaws.services.ec2.model.DeleteKeyPairRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairRequest;
+import com.amazonaws.services.ec2.model.CreateKeyPairResult;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
 import com.amazonaws.services.ec2.model.RebootInstancesRequest;
@@ -35,20 +52,17 @@ import com.amazonaws.services.ec2.model.DescribeImagesResult;
 import com.amazonaws.services.ec2.model.Image;
 import com.amazonaws.services.ec2.model.Filter;
 
+
 public class awsTest {
 
-	static AmazonEC2      ec2;
+	static AmazonEC2 ec2;
 
 	private static void init() throws Exception {
-        String region = "us-east-2"; // 리전을 본인이 사용하는 리전으로 변경
-		
-        System.setProperty("aws.accessKeyId", "AKIARAUQWXEZZK5NQEUV");
-        System.setProperty("aws.secretKey", "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");
-        System.setProperty("aws.region", region); // 리전을 시스템 속성으로 설정
+		//크레딧 key 입력자리(github에 업로드 X)
 
+		
         // 다른 초기화 코드는 그대로 유지됩니다.
-		ec2 = AmazonEC2ClientBuilder.standard()
-			.build();
+		ec2 = AmazonEC2ClientBuilder.standard().build();
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -70,6 +84,10 @@ public class awsTest {
 			System.out.println("  3. start instance               4. available regions      ");
 			System.out.println("  5. stop instance                6. create instance        ");
 			System.out.println("  7. reboot instance              8. list images            ");
+			System.out.println("  9. condor_status                10. start all instance    ");
+			System.out.println("  11. stop all instance           12. terminate instance    ");
+			System.out.println("  13. list SecurityGroups         14. list KeyPairs         ");
+			System.out.println("  15. create KeyPairs             16. delete KeyPairs       ");
 			System.out.println("                                 99. quit                   ");
 			System.out.println("------------------------------------------------------------");
 			
@@ -138,6 +156,51 @@ public class awsTest {
 			case 8: 
 				listImages();
 				break;
+			case 9: 
+				System.out.print("Enter instance id: ");
+				if(id_string.hasNext())
+					instance_id = id_string.nextLine();
+				
+				if(!instance_id.isBlank()) 
+					executeCommandOnInstance(instance_id);
+				break;
+			case 10: 
+				startAllInstances();
+				break;
+			case 11: 
+				stopAllInstances();
+				break;
+			case 12:
+				System.out.print("Enter instance id: ");
+				if(id_string.hasNext())
+					instance_id = id_string.nextLine();
+				
+				if(!instance_id.isBlank()) 
+					terminateEC2(instance_id);
+				break;
+			case 13: 
+				describeAllSecurityGroups();
+				break;
+			case 14: 
+				describeAllKeys();
+				break;
+			case 15: 
+				System.out.print("Enter keyname: ");
+				if(id_string.hasNext())
+					instance_id = id_string.nextLine();
+				
+				if(!instance_id.isBlank()) 
+					createKeyPair(instance_id);
+				break;
+			case 16: 
+				System.out.print("Enter keyname: ");
+				if(id_string.hasNext())
+					instance_id = id_string.nextLine();
+				
+				if(!instance_id.isBlank()) 
+					deleteKeyPair(instance_id);
+				break;
+
 
 			case 99: 
 				System.out.println("bye!");
@@ -149,6 +212,146 @@ public class awsTest {
 
 		}
 		
+	}
+
+	public static void deleteKeyPair(String keyName) {
+        try {
+            DeleteKeyPairRequest request = new DeleteKeyPairRequest().withKeyName(keyName);
+            ec2.deleteKeyPair(request);
+            System.out.println("Key pair deleted successfully: " + keyName);
+        } catch (Exception e) {
+            System.err.println("Error deleting key pair: " + e.getMessage());
+        }
+    }
+
+    public static void createKeyPair(String keyPairName) {
+        try {
+            CreateKeyPairRequest request = new CreateKeyPairRequest()
+                    .withKeyName(keyPairName);
+
+            CreateKeyPairResult response = ec2.createKeyPair(request);
+
+            System.out.println("Key pair created:");
+            System.out.println("Key name: " + response.getKeyPair().getKeyName());
+            System.out.println("Key fingerprint: " + response.getKeyPair().getKeyFingerprint());
+
+            // Save private key material to a file
+            savePrivateKeyMaterial(response.getKeyPair().getKeyName(),response.getKeyPair().getKeyMaterial());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void savePrivateKeyMaterial(String keyName, String Material) {
+        try (FileWriter writer = new FileWriter(keyName+".pem")) {
+            writer.write(Material);
+            System.out.println("Private key material saved " );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void describeAllKeys() {
+        try {
+            DescribeKeyPairsRequest request = new DescribeKeyPairsRequest();
+			int keyNum=0;
+            DescribeKeyPairsResult response = ec2.describeKeyPairs(request);
+            for (KeyPairInfo keyPair : response.getKeyPairs()) {
+				keyNum += 1;
+                System.out.printf("-----------key %d ------------%nkey name : %s %n fingerprint : %s%n",
+                        keyNum, keyPair.getKeyName(), keyPair.getKeyFingerprint());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void describeAllSecurityGroups() {
+        try {
+            DescribeSecurityGroupsRequest request = new DescribeSecurityGroupsRequest();
+
+            DescribeSecurityGroupsResult response = ec2.describeSecurityGroups(request);
+            for (SecurityGroup group : response.getSecurityGroups()) {
+                System.out.println("Security Group ID: " + group.getGroupId());
+                System.out.println("Group Name: " + group.getGroupName());
+                System.out.println("VPC ID: " + group.getVpcId());
+                System.out.println("Description: " + group.getDescription());
+                System.out.println("Ingress Rules: " + group.getIpPermissions());
+                System.out.println("Egress Rules: " + group.getIpPermissionsEgress());
+                System.out.println("----------------------------------");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+	public static void terminateEC2(String instanceId) {
+		TerminateInstancesRequest terminateRequest = new TerminateInstancesRequest()
+				.withInstanceIds(instanceId);
+
+		TerminateInstancesResult terminateResponse = ec2.terminateInstances(terminateRequest);
+		System.out.println("Termination initiated for instance " + instanceId);
+	}
+
+	public static void stopAllInstances() {
+		
+		System.out.println("stop all instances....");
+		boolean done = false;
+		
+		DescribeInstancesRequest request = new DescribeInstancesRequest();
+		
+		while(!done) {
+			DescribeInstancesResult response = ec2.describeInstances(request);
+
+			for(Reservation reservation : response.getReservations()) {
+				for(Instance instance : reservation.getInstances()) {
+					if ("running".equals(instance.getState().getName())){
+						stopInstance(instance.getInstanceId());
+					}
+					else{
+						System.out.println(instance.getInstanceId());
+					}
+				}
+			}
+
+			request.setNextToken(response.getNextToken());
+
+			if(response.getNextToken() == null) {
+				done = true;
+			}
+		}
+	}
+
+
+	public static void startAllInstances() {
+		
+		System.out.println("start all instances....");
+		boolean done = false;
+		
+		DescribeInstancesRequest request = new DescribeInstancesRequest();
+		
+		while(!done) {
+			DescribeInstancesResult response = ec2.describeInstances(request);
+
+			for(Reservation reservation : response.getReservations()) {
+				for(Instance instance : reservation.getInstances()) {
+					if ("stopped".equals(instance.getState().getName())){
+						startInstance(instance.getInstanceId());
+					}
+					else{
+						System.out.println(instance.getInstanceId());
+					}
+				}
+			}
+
+			request.setNextToken(response.getNextToken());
+
+			if(response.getNextToken() == null) {
+				done = true;
+			}
+		}
 	}
 
 	public static void listInstances() {
@@ -319,13 +522,25 @@ public class awsTest {
 	
 	public static void listImages() {
 		System.out.println("Listing images....");
-		
+
 		final AmazonEC2 ec2 = AmazonEC2ClientBuilder.defaultClient();
+	
+		DescribeImagesRequest request = new DescribeImagesRequest();
+		request.getFilters().add(new Filter().withName("name").withValues("aws-htcondor-slave"));
+	
+		DescribeImagesResult results = ec2.describeImages(request);
+	
+		for (Image image : results.getImages()) {
+			System.out.printf("[ImageID] %s, [Name] %s, [Owner] %s\n",
+					image.getImageId(), image.getName(), image.getOwnerId());
+		}
+		/*
+		final AmazonEC2 ec2 = Am1azonEC2ClientBuilder.defaultClient();
 		
 		DescribeImagesRequest request = new DescribeImagesRequest();
 		ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 		
-		request.getFilters().add(new Filter().withName("name").withValues("htcondor-slave-image"));
+		request.getFilters().add(new Filter().withName("name").withValues("aws-htcondor-slave"));
 		request.setRequestCredentialsProvider(credentialsProvider);
 		
 		DescribeImagesResult results = ec2.describeImages(request);
@@ -334,7 +549,60 @@ public class awsTest {
 			System.out.printf("[ImageID] %s, [Name] %s, [Owner] %s\n", 
 					images.getImageId(), images.getName(), images.getOwnerId());
 		}
-		
+		*/
 	}
+
+	public static void executeCommandOnInstance(String instanceId) {
+        try {
+			String command = "condor_status";
+            String privateKeyPath = "C:\\project\\aws_key\\master.pem"; // 개인 키 파일 경로
+            String username = "ec2-user"; // 인스턴스의 사용자 이름
+            String instanceIp = getInstancePublicIp(instanceId);
+
+            if (instanceIp != null) {
+                String sshCommand = String.format(
+				"ssh -i \"%s\" -o StrictHostKeyChecking=no %s@ec2-%s.us-east-2.compute.amazonaws.com \"%s\"",
+				privateKeyPath, username, instanceIp, command);
+				System.out.println(sshCommand);
+                Process process = Runtime.getRuntime().exec(sshCommand);
+
+                printCommandOutput(process.getInputStream());
+                printCommandOutput(process.getErrorStream());
+
+                int exitCode = process.waitFor();
+                System.out.println("Command execution completed with exit code: " + exitCode);
+            } else {
+                System.out.println("Unable to retrieve public IP of the instance with ID: " + instanceId);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getInstancePublicIp(String instanceId) {
+		DescribeInstancesRequest describeInstancesRequest = new DescribeInstancesRequest()
+				.withInstanceIds(instanceId);
+
+		DescribeInstancesResult describeInstancesResult = ec2.describeInstances(describeInstancesRequest);
+
+		for (Reservation reservation : describeInstancesResult.getReservations()) {
+			for (Instance instance : reservation.getInstances()) {
+				if (instance.getInstanceId().equals(instanceId)) {
+					return instance.getPublicIpAddress().replace('.', '-');
+				}
+			}
+		}
+
+		// 해당 인스턴스 ID에 대한 정보를 찾지 못한 경우
+		return null;
+    }
+
+    private static void printCommandOutput(InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
 }
 	
